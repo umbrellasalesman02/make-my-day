67409d63938f36ee2b08558dfa183ef03b163bf5
Erik Wiberg <erik.wiberg@funkismultimedia.se>
2026-03-30 22:08:14 +0200

    add example GreetingService using Effect ServiceMap

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,22 @@
+import { Effect, Layer, ServiceMap } from "effect";
+
 export function fn() {
   return "Hello, tsdown!";
 }
+
+export class GreetingService extends ServiceMap.Service<
+  GreetingService,
+  {
+    readonly greet: (name: string) => Effect.Effect<string>;
+  }
+>()("@app/GreetingService") {
+  static readonly layer = Layer.succeed(GreetingService, {
+    greet: (name: string) => Effect.succeed(`Hello, ${name}!`),
+  });
+}
+
+export const greetingFor = (name: string) =>
+  Effect.gen(function* () {
+    const service = yield* GreetingService;
+    return yield* service.greet(name);
+  });
